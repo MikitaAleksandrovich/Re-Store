60efff81e78d44c53c3de7d71f8d66bbfd17961d
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-11-12 22:18:59 +0300

    Create basic structure of react-redux bookstore App and App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('App is ready!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,6 @@
+
+export default class BookStoreService {
+    getBook() {
+        return [];
+    }
+};
