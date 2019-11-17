8808bb078d67372345a5f6867314609445b67b61
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-11-17 20:18:42 +0300

    Create basic redux components

--- src/components/bookstore-service-context/bookstore-service-context.js
+++ src/components/bookstore-service-context/bookstore-service-context.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const {
-    ProviderL: BookstoreServiceProvider,
+    Provider: BookstoreServiceProvider,
     Consumer: BookstoreServiceConsumer,
 } = React.createContext(); 
 
