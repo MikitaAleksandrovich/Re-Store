2e62e8ef93d033b174e49f3336380cbf4cc4b232
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-12-25 23:15:25 +0300

    refactor updateCartItems and updatecartItem functions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,44 @@ const initialState = {
     orderTotal: 220
 };
 
+const updateCartItems = (cartItems, item, idx) => {
+
+    if(idx === -1) {
+        return [
+            ...cartItems,
+            item
+        ];
+    } 
+
+    return [
+        ...cartItems.slice(0, idx),
+        item,
+        ...cartItems.slice(idx + 1),
+    ];
+};
+
+const updateCartItem = (book, item) => {
+    let newItem;
+
+    if(item) {
+        return {
+            ...item,
+            count: item.count + 1,
+            total: item.total + book.price,
+        }
+
+    } else {
+        return {
+            id: book.id,
+            title: book.title,
+            count: 1,
+            total: book.price
+        };
+    }
+}
+
+
+
 const reducer = (state = initialState, action) => {
     
     switch(action.type) {
@@ -40,44 +78,13 @@ const reducer = (state = initialState, action) => {
             const book = state.books.find((book) => book.id === bookId);
             const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
             const item = state.cartItems[itemIndex];
-            
-            let newItem;
 
-            if(item) {
-                newItem = {
-                    ...item,
-                    count: item.count + 1,
-                    total: item.total + book.price,
-                }
-
-            } else {
-                newItem = {
-                    id: book.id,
-                    title: book.title,
-                    count: 1,
-                    total: book.price
-                };
-            }
-
+            const newItem = updateCartItem(book, item);
         
-            if(itemIndex < 0) {
-                return {
-                    ...state,
-                    cartItems: [
-                    ...state.cartItems,
-                    newItem
-                    ]
-                };
-            } else {
-                return {
-                    ...state,
-                    cartItems: [
-                        ...state.cartItems.slice(0, itemIndex),
-                        newItem,
-                        ...state.cartItems.slice(itemIndex + 1),
-                    ]
-                };
-            }
+            return {
+                ...state,
+                cartItems: updateCartItems(state.cartItems, newItem, itemIndex),
+            };
       
 
         default: 
